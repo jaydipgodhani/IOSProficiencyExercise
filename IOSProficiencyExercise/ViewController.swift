10fbaa5c4b9018f81ecd5848bc50310e6daecfe9
Jaydip Godhani <godhani.jaydip145@gmail.com>
2021-06-13 10:57:22 +0530

    Initial Commit

---
+++ IOSProficiencyExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSProficiencyExercise
+//
+//  Created by Jaydip Godhani on 13/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
